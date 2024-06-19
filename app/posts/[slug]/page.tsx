f8b33fefc84a5ebf0557d604f56075bee253a130
Firzus <92395888+Firzus@users.noreply.github.com>
2024-06-20 01:13:58 +0200

    refactor: update PostPage function signature to include type for params

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,12 @@
 import prisma from "@lib/db";
 
-export default async function PostPage({ params }) {
+export default async function PostPage({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}) {
   const post = await prisma.post.findUnique({
     where: { slug: params.slug },
   });
